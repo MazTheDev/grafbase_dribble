bcf44bb33bb304cad46d0b8ba6aaf80b51cd47b1
MazTheDev <138598705+MazTheDev@users.noreply.github.com>
2023-07-21 19:54:22 +0200

    Dribble_data

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import Navbar from '@/components/Navbar'
+import './global.css'
+import Footer from '@/components/Footer'
+
+export const metadata = {
+  title: 'Flexibble',
+  description: 'Showcase and discover remarable',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
+
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <section className="flex-start flex-col
+        lg:px-20 py-6 px-5 mb-16">
+            <h1>Categories</h1>
+            <h1>Post</h1>
+            <h1>LoadMore</h1>
+        </section>
+    )
+}
+
+export default Home;
